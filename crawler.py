cc3a18ea71eb4af9e2eed0c978af8630a7176dfa
joaop221 <joaopedro08barbozagarcia@gmail.com>
2017-05-22 21:23:29 -0300

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,24 @@
+from django import *
+
+
+class crawler:
+    def __init__(self,url, key):
+        self.url = url
+        self.key = key
+        
+
+    def request():
+        return json_url
+        
+
+def main():
+    #para efeito de testes
+    crawler("https://www.usesolution.com.br","doces")
+    json_url = crawler.request()
+    print(json_url)
+    
+
+if __name__ == "__main__":
+    main()
+        
+    
